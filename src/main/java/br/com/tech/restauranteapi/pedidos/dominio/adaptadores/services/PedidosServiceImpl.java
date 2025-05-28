de4596b310fa743829c290c7dd83589ca4fd0fad
Marcelo Benjamim <mah.benjamim12@gmail.com>
2025-05-27 21:44:02 -0300

    Agrupando produtos

--- src/main/java/br/com/tech/restauranteapi/pedidos/dominio/adaptadores/services/PedidosServiceImpl.java
+++ src/main/java/br/com/tech/restauranteapi/pedidos/dominio/adaptadores/services/PedidosServiceImpl.java
@@ -5,10 +5,7 @@ import br.com.tech.restauranteapi.associacaoPedidoProduto.dominio.AssociacaoProd
 import br.com.tech.restauranteapi.associacaoPedidoProduto.dominio.portas.interfaces.AssociacaoPedidoProdutoServicePort;
 import br.com.tech.restauranteapi.clientes.dominio.portas.repositories.ClienteRepositoryPort;
 import br.com.tech.restauranteapi.pedidos.dominio.Pedido;
-import br.com.tech.restauranteapi.pedidos.dominio.dtos.CriarPedidoDto;
-import br.com.tech.restauranteapi.pedidos.dominio.dtos.PedidoDto;
-import br.com.tech.restauranteapi.pedidos.dominio.dtos.PedidoResponseDto;
-import br.com.tech.restauranteapi.pedidos.dominio.dtos.ProdutoPedidoResponseDto;
+import br.com.tech.restauranteapi.pedidos.dominio.dtos.*;
 import br.com.tech.restauranteapi.pedidos.dominio.portas.interfaces.PedidosServicePort;
 import br.com.tech.restauranteapi.pedidos.infraestrutura.repositories.PedidosRepository;
 import br.com.tech.restauranteapi.produtos.dominio.Produto;
@@ -21,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -45,17 +43,7 @@ public class PedidosServiceImpl implements PedidosServicePort {
         // Salva o pedido primeiro para ter o ID
         Pedido pedidoSalvo = pedidosRepository.salvar(pedido);
 
-        List<AssociacaoPedidoProduto> associacoes = dto.getProdutos().stream()
-                .map(produtoDto -> {
-                    Produto produto = produtosPort.buscarPorId(produtoDto.getProdutoId());
-                    return AssociacaoPedidoProduto.builder()
-                            .produto(produto)
-                            .pedido(pedidoSalvo)
-                            .quantidade(produtoDto.getQuantidade())
-                            .preco(produto.getPreco().multiply(BigDecimal.valueOf(produtoDto.getQuantidade())))
-                            .build();
-                } )
-                .toList();
+        List<AssociacaoPedidoProduto> associacoes = agruparProdutos(dto, pedidoSalvo);
 
         List<AssociacaoPedidoProduto> associcaoProdutos = associacaoPedidoProdutoService.salvarTodas(associacoes);
         pedidoSalvo.setAssociacoes(associcaoProdutos.stream().map(AssociacaoProduto::builderAssociacao).collect(Collectors.toList()));
@@ -83,5 +71,29 @@ public class PedidosServiceImpl implements PedidosServicePort {
     public Page<PedidoDto> listarFilaPedidos(Pageable pageable) {
         return pedidosRepository.listarFilaPedidos(pageable)
                 .map(Pedido::toPedidosDto);
+    }
+
+    private List<AssociacaoPedidoProduto> agruparProdutos(CriarPedidoDto dto, Pedido pedidoSalvo){
+         Map<Integer, Integer> quantidadePorProduto = dto.getProdutos().stream()
+                .collect(Collectors.groupingBy(
+                        ProdutoPedidoDto::getProdutoId,
+                        Collectors.summingInt(ProdutoPedidoDto::getQuantidade)
+                ));
+
+        return quantidadePorProduto.entrySet().stream()
+                .map(entry -> {
+                    Integer produtoId = entry.getKey();
+                    Integer quantidadeTotal = entry.getValue();
+
+                    Produto produto = produtosPort.buscarPorId(produtoId);
+
+                    return AssociacaoPedidoProduto.builder()
+                            .produto(produto)
+                            .pedido(pedidoSalvo)
+                            .quantidade(quantidadeTotal)
+                            .preco(produto.getPreco().multiply(BigDecimal.valueOf(quantidadeTotal)))
+                            .build();
+                })
+                .toList();
     }
 }
