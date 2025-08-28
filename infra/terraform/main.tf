1d99b97fd5cd0c57bedbadb3a7f51aeaa9aadf37
Marcelo Benjamim <mah.benjamim12@gmail.com>
2025-08-28 08:55:44 -0300

    nova infra e novo svc

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,55 +5,35 @@ data "aws_vpc" "postech_vpc" {
   }
 }
 
-data "aws_subnet" "public_a" {
+data "aws_subnet" "private_a" {
   filter {
     name   = "tag:Name"
-    values = ["public-a"]
+    values = ["private-a"]
   }
 }
 
-data "aws_subnet" "public_b" {
+data "aws_subnet" "private_b" {
   filter {
     name   = "tag:Name"
-    values = ["public-b"]
+    values = ["private-b"]
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole-estudo"
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg"
+  description = "Security Group for EKS Cluster"
+  vpc_id      =  data.aws_vpc.postech_vpc.id
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_cluster" "fargate_cluster" {
-  name = "estudo-cluster"
-}
-
-
-resource "aws_security_group" "ecs_sg" {
-  vpc_id = data.aws_vpc.postech_vpc.id
+  # Permite comunicação do cluster para os nós
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    description = "Allow EKS Control Plane"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # Saída liberada
   egress {
     from_port   = 0
     to_port     = 0
@@ -62,121 +42,55 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "restaurante_log_group" {
-  name              = "/ecs/restaurante-logs"
-  retention_in_days = 7
+
+resource "aws_eks_access_entry" "eks-access-entry" {
+  cluster_name      = aws_eks_cluster.eks_cluster_restaurante.name
+  principal_arn     = var.principalArn
+  kubernetes_groups = ["fiap"]
+  type              = "STANDARD"
 }
 
+resource "aws_eks_access_policy_association" "eks-access-policy" {
+  cluster_name  = aws_eks_cluster.eks_cluster_restaurante.name
+  policy_arn    = var.policyArn
+  principal_arn = var.principalArn
 
-resource "aws_ecs_task_definition" "restaurante_task" {
-  family                   = "restaurante-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name  = "restaurante-ecs"
-      image = "617618117752.dkr.ecr.us-east-1.amazonaws.com/restaurante-app:latest"
-      cpu   = 256
-      memory = 512
-      essential = true
-       environment : [
-        {
-          name : "DB_HOST",
-          value : "postgres-restaurante.ckzwimcqm9la.us-east-1.rds.amazonaws.com"
-        },
-        {
-          name : "DB_NAME",
-          value : "postgres_restaurante"
-        },
-        {
-          name : "DB_USER",
-          value : "adminuser"
-        },
-        {
-          name : "DB_PASSWORD",
-          value : "SenhaForte123!"
-        }
-      ],
-      portMappings = [
-        {
-          containerPort = 8080  
-          hostPort      = 8080 
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.restaurante_log_group.name
-          awslogs-region        = "us-east-1"
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-    }
-  ])
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_logs_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-}
-
-
-resource "aws_ecs_service" "restaurante_ecs_service" {
-  name            = "restaurante-ecs-service"
-  cluster         = aws_ecs_cluster.fargate_cluster.id
-  task_definition = aws_ecs_task_definition.restaurante_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id]
-    security_groups  = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.restaurante_target_group.arn
-    container_name   = "restaurante-ecs"
-    container_port   = 8080  # Porta que o container está ouvindo
+  access_scope {
+    type = "cluster"
   }
 }
 
-# Criar o NLB
-resource "aws_lb" "restaurante_nlb" {
-  name               = "restaurante-nlb"
-  internal           = false  # Deixe false para ser acessível da internet
-  load_balancer_type = "network"  # Network Load Balancer
-  subnets            = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id]  # Subnet pública
-}
+# 8. Cluster EKS
+resource "aws_eks_cluster" "eks_cluster_restaurante" {
+  name     = "eks-fargate-eks_cluster_restaurante"
+  role_arn = var.labRole
 
-# Target group para o ECS
-resource "aws_lb_target_group" "restaurante_target_group" {
-  name        = "restaurante-target-group"
-  port        = 8080  # Porta usada pelo container
-  protocol    = "TCP"  # Use TCP para um NLB
-  vpc_id      = data.aws_vpc.postech_vpc.id
-  target_type = "ip"
+  vpc_config {
+    subnet_ids = [data.aws_subnet.private_a.id, data.aws_subnet.private_b.id]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
 
-  health_check {
-    interval            = 30
-    path                = "/"  # Ajuste conforme o endpoint da aplicação
-    protocol            = "HTTP"  # Isso é importante para health checks
-    matcher             = "200-399"
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+  access_config {
+    authentication_mode = var.accessConfig
   }
 }
 
-resource "aws_lb_listener" "restaurante_nlb_listener" {
-  load_balancer_arn = aws_lb.restaurante_nlb.arn
-  port              = 80  # O listener escuta na porta 80 para tráfego externo
-  protocol          = "TCP"  # Para HTTP
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.restaurante_target_group.arn
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster_restaurante.name
+  node_group_name = var.nodeGroup
+  node_role_arn   = var.labRole
+  subnet_ids      = [data.aws_subnet.private_a.id, data.aws_subnet.private_b.id]
+  disk_size       = 50
+  instance_types  = [var.instanceType]
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
   }
 }
+
